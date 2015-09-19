81e17b29094bf3dd218e37656536856a7b839162
CeceXX <cesaredecal@v1021-wn-14-162.campus-dynamic.uwaterloo.ca>
2015-09-19 00:25:58 -0400

    Added Xcode 7.0 project

---
+++ SilverApp/SilverApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SilverApp
+//
+//  Created by Cesare de Cal on 19/09/15.
+//  Copyright © 2015 SilverApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
